28da73d3acff397a159de83675f12b7e917f1e60
David <ignacio.g4l4rr4g4@gmail.com>
2025-03-27 16:11:20 -0300

    El gatito se puede prender y apagar

--- Funciones.js
+++ Funciones.js
@@ -1,3 +1,9 @@
 let botonPrender = document.getElementById("BotonPrender")
+let botonApagar = document.getElementById("botonaPagar")
+let imagenGato = document.getElementById("gatoImagen")
 botonPrender.onclick = function(){
+    imagenGato.src="./Imagenes/img_GatitoEncendido.png"
+}
+botonApagar.onclick = function(){
+    imagenGato.src="./Imagenes/img_Gatito.png"
 }
