86d021521c842ee773fa5ea8793a89f9bb6b1ba4
David <ignacio.g4l4rr4g4@gmail.com>
2025-03-28 12:19:18 -0300

    constantes en la función

--- JavaScript/Funciones.js
+++ JavaScript/Funciones.js
@@ -8,7 +8,7 @@ let persona1 = {                                 //objeto
     apellido : "Galarraga",
     dni: "45201414",
     edad : 29,
-    colores: ["blanco","amarillo","rojo"]
+    colores: ["blanco","azul","rojo"]
 }
 
 let persona2 = {                                 //objeto              
@@ -16,19 +16,21 @@ let persona2 = {                                 //objeto
     apellido : "Galarraga",
     dni: "46201414",
     edad : 30,
-    colores: ["blanco","amarillo","rojo"]
+    colores: ["blanco","azul","rojo"]
 }
 
 
 
 function comparar (persona1,persona2){
-    if ( (persona1.colores.includes("azul")) && (persona2.colores.includes("azul")) ){
+    const p1Azul = persona1.colores.includes("azul")
+    const p2Azul = persona2.colores.includes("azul")
+    if ( p1Azul && p2Azul ){
         console.log(persona1.nombre," y ",persona2.nombre, " tienen azul")
-    } else if (persona1.colores.includes("azul")) {
+    } else if (p1Azul) {
         console.log(persona1.nombre, " tiene azul")
-    } else if (persona2.colores.includes("azul")) {
+    } else if (p2Azul) {
         console.log(persona2.nombre, " tiene azul")
-    } else if ((!persona1.colores.includes("azul")) && (!persona2.colores.includes("azul"))) {
+    } else if (!p1Azul && !p2Azul) {
         console.log(persona1.nombre," y ",persona2.nombre, " no tienen azul")
     }
 }
