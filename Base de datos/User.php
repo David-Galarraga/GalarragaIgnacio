83023162e3d3e9ddd97f687bc54d5c8eaa19f734
David <ignacio.g4l4rr4g4@gmail.com>
2025-09-19 18:05:06 -0300

    arreglos

--- Base de datos/User.php
+++ Base de datos/User.php
@@ -5,20 +5,20 @@ class User {
     public function __construct() {
         $this->db = (new Database())->connect();
     }
-    public function add($password, $nombre, $apellido, $nickname) {
-        $sql = "INSERT INTO users (password, nombre, apellido, nickname) VALUES (:password, :nombre, :apellido, :nickname)";
+    public function add($password, $nombre, $apellido, $nickname, $rol) {
+        $sql = "INSERT INTO users (password, nombre, apellido, nickname, rol) VALUES (:password, :nombre, :apellido, :nickname. :rol)";
         $stmt = $this->db->prepare($sql);
-        $stmt->execute([`:password` => $password, `:nombre` => $nombre, `:apellido` => $apellido, `:nickname` => $nickname]);
+        $stmt->execute([`:password` => $password, `:nombre` => $nombre, `:apellido` => $apellido, `:nickname` => $nickname, `:rol` => $rol]);
     }
     public function getAll() {
         $sql = "SELECT * FROM usarios";
         $stmt = $this->db->query($sql);
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
-    public function update($id, $password, $nombre, $apellido, $nickname) {
-        $sql = "UPDATE users SET password = :password, nombre = :nombre, apellido = :apellido, nickname = :nickname WHERE id = :id";
+    public function update($id, $password, $nombre, $apellido, $nickname, $rol) {
+        $sql = "UPDATE users SET password = :password, nombre = :nombre, apellido = :apellido, nickname = :nickname, rol = :rol WHERE id = :id";
         $stmt = $this->db->prepare($sql);
-        $stmt->execute([`:password` => $password, `:nombre` => $nombre, `:apellido` => $apellido, `:nickname` => $nickname, ':id' => $id]);
+        $stmt->execute([`:password` => $password, `:nombre` => $nombre, `:apellido` => $apellido, `:nickname` => $nickname, `:rol` => $rol , ':id' => $id]);
     }
     public function delete($id) {
         $sql = "DELETE FROM users WHERE id = :id";
---
+++ Base de datos/listar.php
@@ -0,0 +1,7 @@
+<?php
+require_once "User.php";
+$user = new User();
+$users = $user->getAll();
+foreach ($users as $u) {
+    echo $u['id'] . " - " . $u['nombre'] . " " . $u['apellido'] . "<br>";
+}
