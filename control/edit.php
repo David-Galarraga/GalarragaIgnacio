64830916035a9086a63af47d105cd1a05a7d61cc
Juan <juan_dlp2@outlook.com>
2025-11-08 19:25:49 -0300

    editUsuario actualizado con hash

--- control/edit.php
+++ control/edit.php
@@ -1,14 +1,17 @@
 <?php
-require_once "../entidades/User.php";
-require_once "../middleWork/controlarSesiones.php";
-if (!$_SESSION["datos"]["rol"] === "Admin") {
-        header("Location: ../index.php");
-        exit();
-    }
-$user = new User();
-$user->update($_POST["id"], $_POST["password"], $_POST["nombre"], $_POST["apellido"], $_POST["email"], $_POST["rol"]);
+    require_once "../entidades/User.php";
+    require_once "../middleWork/controlarSesiones.php";
+    if (!$_SESSION["datos"]["rol"] === "Admin") {
+            header("Location: ../index.php");
+            exit();
+        }
 
-echo "Usuario actualizado." . "\n";
-echo "<button onclick=\"window.location.href='../vista/bienvenidoAdmin.php'\">Volver</button>";
+    $passCrypt = password_hash($_POST["password"], PASSWORD_DEFAULT);
+    
+    $user = new User();
+    $user->update($_POST["id"], $passCrypt, $_POST["nombre"], $_POST["apellido"], $_POST["email"], $_POST["rol"]);
+
+    echo "Usuario actualizado." . "\n";
+    echo "<button onclick=\"window.location.href='../vista/bienvenidoAdmin.php'\">Volver</button>";
 
 ?>
